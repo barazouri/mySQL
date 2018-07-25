8ba0c1f67c3f5dee9752fee6d7874dff784f4f66
Bar Azouri <barazouri24@gmail.com>
2018-07-25 10:38:23 +0300

    Initial Commit

---
+++ book_store_db/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  book_store_db
+//
+//  Created by bar azouri on 25/07/2018.
+//  Copyright © 2018 bar azouri. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
